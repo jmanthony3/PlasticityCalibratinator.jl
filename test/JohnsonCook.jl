2f853d82ec64b12f94cda0fab83ce46c2b12f4f2
Joby M. Anthony III <jmanthony1@liberty.edu>
2024-11-06 11:21:21 -0500

    suppress some print statements from debugging

--- test/JohnsonCook.jl
+++ test/JohnsonCook.jl
@@ -266,7 +266,7 @@ end
 function PlasticityCalibratinator.plot_sets!(::Type{JC}, modelcalibration)
     # ax, dataseries, Scale_MPa
     for i in range(1, modelcalibration[].modeldata.nsets)
-        println(i)
+        # println(i)
         scatter!(modelcalibration[].ax,    @lift(Point2f.($(modelcalibration[].dataseries[1][i]).x, $(modelcalibration[].dataseries[1][i]).y)),
             color=i, colormap=:viridis, colorrange=(1, modelcalibration[].modeldata.nsets), label="Data - " * modelcalibration[].modeldata.test_cond["Name"][i])
         lines!(modelcalibration[].ax,      @lift(Point2f.($(modelcalibration[].dataseries[2][i]).x, $(modelcalibration[].dataseries[2][i]).y .* modelcalibration[].modeldata.stressscale)),
