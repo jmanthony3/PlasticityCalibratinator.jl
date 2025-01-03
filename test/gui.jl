bf4a35b8377535902fb2fcd86fc4d9be15a08c6f
Joby M. Anthony III <jmanthony1@liberty.edu>
2025-01-03 11:04:10 -0500

    punted to extension

--- test/gui.jl
+++ test/gui.jl
@@ -2,8 +2,5 @@ using PlasticityCalibratinator
 include("JohnsonCook.jl")
 
 using BammannChiesaJohnsonPlasticity
-include("BCJMetal.jl")
-include("DK.jl")
-include("Bammann1990Modeling.jl")
 
 GLMakie.closeall(); main()
